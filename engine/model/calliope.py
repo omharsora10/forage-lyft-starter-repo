6d93992edd89fa8fb8bbe6e3dea47328fecf507a
omharsora10 <130318317+omharsora10@users.noreply.github.com>
2023-04-11 20:05:29 +0530

    Update calliope.py

--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -1,12 +1,11 @@
-from datetime import datetime
-
+from datetime import datetime, timedelta
 from engine.capulet_engine import CapuletEngine
 
 
 class Calliope(CapuletEngine):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
+        service_threshold_date = self.last_service_date + timedelta(days=730)
+        if service_threshold_date <= datetime.today().date() or self.engine_should_be_serviced():
             return True
         else:
             return False
