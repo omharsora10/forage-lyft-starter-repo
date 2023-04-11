18786d5636066e1decdc2239a19ffcca41ac7d00
omharsora10 <130318317+omharsora10@users.noreply.github.com>
2023-04-11 18:12:11 +0530

    Update capulet_engine.py

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,15 @@
 from abc import ABC
 
-from car import Car
 
-
-class CapuletEngine(Car, ABC):
-    def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
-        self.current_mileage = current_mileage
+class Engine(ABC):
+    def __init__(self, last_service_mileage: int, current_mileage: int):
         self.last_service_mileage = last_service_mileage
+        self.current_mileage = current_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self) -> bool:
         return self.current_mileage - self.last_service_mileage > 30000
+
+
+class CapuletEngine(Engine):
+    def __init__(self, last_service_mileage: int, current_mileage: int):
+        super().__init__(last_service_mileage, current_mileage)
