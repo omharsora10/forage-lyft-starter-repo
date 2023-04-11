0dd539d44fc6f4ca9c4978e82eee32fce0a672d1
omharsora10 <130318317+omharsora10@users.noreply.github.com>
2023-04-11 20:25:13 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,9 +1,8 @@
 from abc import ABC, abstractmethod
 
-
 class Car(ABC):
     def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+        self._last_service_date = last_service_date
 
     @abstractmethod
     def needs_service(self):
