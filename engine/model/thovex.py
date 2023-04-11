9cf8671b82869f48c4da5ede9b83e648dd7669ea
omharsora10 <130318317+omharsora10@users.noreply.github.com>
2023-04-11 20:12:08 +0530

    Update thovex.py

--- engine/model/thovex.py
+++ engine/model/thovex.py
@@ -6,7 +6,7 @@ from engine.capulet_engine import CapuletEngine
 class Thovex(CapuletEngine):
     def needs_service(self):
         service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        mileage_threshold = self.last_service_mileage + 100000
+        current_date = datetime.today().date()
+
+        return current_date > service_threshold_date or self.current_mileage > mileage_threshold
